fbd4637816448380f2a38a91325bf61defdcb167
Mohammad Abdul Hadi <hadi@alumni.ubc.ca>
2020-02-05 20:07:10 -0800

    Updated

--- app.R
+++ app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 # Define UI for application that draws a histogram
@@ -30,18 +21,37 @@ ui <- fluidPage(
                      "Number of bins:",
                      min = 1,
                      max = 50,
-                     value = 30)
+                     value = 30),
+        sliderInput("n",
+                    "Number of image:",
+                    min = 1,
+                    max = 10,
+                    value = 1),
+        fileInput("file1", "Choose JPG or PNG File",
+                  multiple = FALSE,
+                  accept = c("image/jpeg", 
+                             "image/png",
+                             "image/svg+xml"))
+        
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+        tabsetPanel(
+          tabPanel("Plot", plotOutput("distPlot")), #, textOutput("selected_var")
+          tabPanel("Images", imageOutput("myImage")), 
+          tabPanel("Browsed Images", imageOutput("browsedImage")) 
+          # tabPanel("Table", tableOutput("table"))
+        )
+         # plotOutput("distPlot"),
+         # Use imageOutput to place the image on the page
+         # imageOutput("myImage")
       )
    )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
    
    output$distPlot <- renderPlot({
        #referring input p in ui.r as input$p
@@ -64,6 +74,44 @@ server <- function(input, output) {
       
       # draw the histogram with the specified number of bins
       hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   })
+   
+   output$myImage <- renderImage({
+     # width  <- session$clientData$output_myImage_width
+     # height <- session$clientData$output_myImage_height
+     
+     width  <- 500
+     height <- 400
+     
+     # When input$n is 3, filename is ./images/image3.png
+     filename <- normalizePath(file.path('./images',
+                                         paste('image', input$n, '.jpg', sep='')))
+     
+     # Return a list containing the filename and alt text
+     list(src = filename,
+          width = width,
+          height = height,
+          alt = paste("Image number", input$n))
+     
+   }, deleteFile = FALSE)
+   
+   output$browsedImage <- renderImage({
+     # width  <- session$clientData$output_myImage_width
+     # height <- session$clientData$output_myImage_height
+     
+     width  <- 500
+     height <- 400
+     
+     # Return a list containing the filename and alt text
+     list(src = input$file1$datapath,
+          width = width,
+          height = height,
+          alt = paste("Random Image"))
+     
+   }, deleteFile = FALSE)
+   
+   output$selected_var <- renderText({ 
+     input$file1$datapath
    })
 }
 
